b29ca8cc52f2b95a7aa84de730b58bf00fb7228b
blue-berry-s <95601084+blue-berry-s@users.noreply.github.com>
2025-09-29 20:31:12 -0400

    Initial Commit

---
+++ drop Tables.sql
@@ -0,0 +1,19 @@
+Drop TABLE Account;
+
+Drop TABLE ADMIN;
+
+Drop TABLE Customer;
+
+Drop TABLE CustomerDeals;
+
+Drop TABLE Deals;
+
+Drop TABLE Movie;
+
+Drop Table OrderDetails;
+
+Drop TABLE Orders;
+
+Drop TABLE Review;
+
+Drop TABLE StorePromotions;
