20aa97f2117ba17ed4abf7c849fe389ff8fa11b0
blue-berry-s <95601084+blue-berry-s@users.noreply.github.com>
2025-09-30 14:35:35 -0400

    Filled in Promotion Targets

---
+++ fill Promotion Targets.sql
@@ -0,0 +1,131 @@
+-- Pormotion for all movies
+INSERT INTO PROMOTIONTARGETS (
+    TARGETTYPE, TARGETVALUE
+  ) VALUES (
+    'Genre',
+    'All'
+  );
+
+
+-- Pormotion Types based on Genre Of Movies
+INSERT INTO PROMOTIONTARGETS (
+    TARGETTYPE, TARGETVALUE
+  ) VALUES (
+    'Genre',
+    'Horror'
+  );
+
+INSERT INTO PROMOTIONTARGETS VALUES (
+    'Genre',
+    'Comedy'
+  );
+
+  INSERT INTO PROMOTIONTARGETS (
+    TARGETTYPE, TARGETVALUE
+  ) VALUES (
+    'Genre',
+    'Mystery/Thriller'
+  );
+
+  INSERT INTO PROMOTIONTARGETS (
+    TARGETTYPE, TARGETVALUE
+  ) VALUES (
+    'Genre',
+    'Adventure'
+  );
+
+  INSERT INTO PROMOTIONTARGETS (
+    TARGETTYPE, TARGETVALUE
+  ) VALUES (
+    'Genre',
+    'Family/Kids'
+  );
+
+  INSERT INTO PROMOTIONTARGETS (
+    TARGETTYPE, TARGETVALUE
+  ) VALUES (
+    'Genre',
+    'Musical'
+  );
+
+  INSERT INTO PROMOTIONTARGETS (
+    TARGETTYPE, TARGETVALUE
+  ) VALUES (
+    'Genre',
+    'Romance'
+  );
+
+  INSERT INTO PROMOTIONTARGETS (
+    TARGETTYPE, TARGETVALUE
+  ) VALUES (
+    'Genre',
+    'Fantasy'
+  );
+
+  INSERT INTO PROMOTIONTARGETS (
+    TARGETTYPE, TARGETVALUE
+  ) VALUES (
+    'Genre',
+    'Holiday'
+  );
+
+-- Pormotion Types based on Director
+  INSERT INTO PROMOTIONTARGETS (
+    TARGETTYPE, TARGETVALUE
+  ) VALUES (
+    'Director',
+    'Christopher Nolan'
+  );
+
+  INSERT INTO PROMOTIONTARGETS (
+    TARGETTYPE, TARGETVALUE
+  ) VALUES (
+    'Director',
+    'Jordan Peele'
+  );
+
+
+  INSERT INTO PROMOTIONTARGETS (
+    TARGETTYPE, TARGETVALUE
+  ) VALUES (
+    'Director',
+    'Damien Chazelle'
+  );
+
+-- Pormotion Types based on Price Of Movies
+INSERT INTO PROMOTIONTARGETS (
+    TARGETTYPE, TARGETVALUE
+  ) VALUES (
+    'Price',
+    '1.99'
+  );
+
+INSERT INTO PROMOTIONTARGETS (
+    TARGETTYPE, TARGETVALUE
+  ) VALUES (
+    'Price',
+    '2.99'
+  );
+
+  INSERT INTO PROMOTIONTARGETS (
+    TARGETTYPE, TARGETVALUE
+  ) VALUES (
+    'Price',
+    '3.99'
+  );
+
+  INSERT INTO PROMOTIONTARGETS (
+    TARGETTYPE, TARGETVALUE
+  ) VALUES (
+    'Price',
+    '4.99'
+  );
+  
+
+
+
+
+
+
+
+
